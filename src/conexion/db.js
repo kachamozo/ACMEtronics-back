c16f342d9eefc7395efc39dc43ee7faafe6be6e4
Rodrigo <rodrigodamianmartinez2001@gmail.com>
2022-12-06 11:34:22 -0300

    DB

--- src/conexion/db.js
+++ src/conexion/db.js
@@ -1,11 +1,17 @@
-const { Sequelize } = require('sequelize');
+const {
+	Sequelize
+} = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const {
+	DB_USER,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_NAME
+} = process.env;
 
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-	{
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 	}
@@ -18,7 +24,7 @@ const modelDefiners = [];
 fs.readdirSync(path.join(__dirname, '../models'))
 	.filter(
 		(file) =>
-			file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+		file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
 	)
 	.forEach((file) => {
 		modelDefiners.push(require(path.join(__dirname, '../models', file)));
