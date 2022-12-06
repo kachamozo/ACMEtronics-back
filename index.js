255b6c14f05a40f70689412b1edcf7013025b449
Kachamozo <cachamocito@yahoo.com>
2022-12-05 20:09:26 -0400

    BoilerPlate

---
+++ index.js
@@ -0,0 +1,29 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
+const server = require('./src/conexion/app.js');
+const { conn } = require('./src/conexion/db.js');
+
+// Syncing all the models at once
+conn.sync({ force: false }).then(() => {
+	server.listen(process.env.PORT || 8000, () => {
+		console.log('%s listening at' + process.env.PORT || 8000); // eslint-disable-line no-console
+	});
+});
