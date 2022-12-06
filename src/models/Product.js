3347e8d9284d664ac89d805ea55bbc9665f48a42
Rodrigo <rodrigodamianmartinez2001@gmail.com>
2022-12-06 11:43:30 -0300

    models

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,16 @@
-const { DataTypes } = require('sequelize');
+const {
+	DataTypes
+} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
 	// defino el modelo
 	sequelize.define('product', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true
+		},
 		name: {
 			type: DataTypes.STRING,
 			allowNull: false,
