224a10578fdfcc9b6b27bc4791f7e26626a6edbf
Rodrigo <rodrigodamianmartinez2001@gmail.com>
2022-12-07 11:14:45 -0300

    Add models

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,12 +1,48 @@
-const { DataTypes } = require('sequelize');
+const {
+	DataTypes
+} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
 	// defino el modelo
 	sequelize.define('product', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			primaryKey: true
+		},
 		name: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-	});
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		brand: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		os: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		rating: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},{timestamps: true,
+	paranoid: true,});
 };
--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,31 @@
-const { DataTypes } = require('sequelize');
+const {
+    DataTypes
+} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-	// defino el modelo
-	sequelize.define('user', {
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-	});
+    // defino el modelo
+    sequelize.define('user', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },{timestamps: true,
+        paranoid: true,});
 };
