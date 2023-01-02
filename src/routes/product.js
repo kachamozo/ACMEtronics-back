dd95b456485326903b0131b533697f5ff6a233a7
ErikaJPB <erikajpinedab@gmail.com>
2023-01-02 08:04:14 -0500

    removed verify token from the create product route

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,21 +1,21 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const {
-	getAll,
-	getById,
-	create,
-	update,
-	deleteById,
-	createBulk,
-} = require('../controllers/product');
-const verifyToken = require('../middleware/verifyToken');
+  getAll,
+  getById,
+  create,
+  update,
+  deleteById,
+  createBulk,
+} = require("../controllers/product");
+const verifyToken = require("../middleware/verifyToken");
 
 const router = Router();
 
-router.get('/', getAll);
-router.get('/:id', getById);
-router.post('/', verifyToken, create);
-router.put('/:id', update);
-router.delete('/:id', deleteById);
-router.post('/bulk', createBulk);
+router.get("/", getAll);
+router.get("/:id", getById);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", deleteById);
+router.post("/bulk", createBulk);
 
 module.exports = router;
