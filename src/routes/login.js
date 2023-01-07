1a13ab4450b283f1856cf816fdabacd5dde8019f
Kachamozo <cachamocito@yahoo.com>
2023-01-06 21:34:35 -0400

    fix routes login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
-const { loginUser, profileUser } = require("./../Controllers/login");
+const { loginUser, profileUser } = require('./../controllers/login');
 
-router.post("/login", loginUser);
+router.post('/login', loginUser);
 
-router.get("/profile", profileUser);
+router.get('/profile', profileUser);
 
 module.exports = router;
