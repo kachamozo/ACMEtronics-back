f65399a08e34f43bacce5e0ad36ab823e40197f7
ErikaJPB <erikajpinedab@gmail.com>
2023-02-26 16:32:15 -0500

    db deployed

--- index.js
+++ index.js
@@ -17,13 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
-const server = require('./src/connection/app.js');
-const { conn } = require('./src/connection/db.js');
+require("dotenv").config();
+const server = require("./src/connection/app.js");
+const { conn } = require("./src/connection/db.js");
 
 // Syncing all the models at once
 conn.sync({ force: false }).then(() => {
-	server.listen(process.env.PORT || 8000, () => {
-		console.log('%s listening at ' + process.env.PORT || 8000); // eslint-disable-line no-console
-	});
+  server.listen(process.env.PORT || 8000, () => {
+    console.log("%s listening at " + process.env.PORT || 8000); // eslint-disable-line no-console
+  });
 });
